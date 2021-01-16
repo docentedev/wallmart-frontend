16b4096e2db08bb85c4f9a6dd5d7341337d68412
Claudio Rojas <cerojasr@falabella.cl>
2021-01-16 20:21:02 -0300

    Add cart store into Product and Home components

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,15 +1,19 @@
-import styles from './Product.module.css'
 import { Plus as PlusIcon, Minus as MinusIcon } from 'react-feather';
 import { currencyFormat } from '../../utils'
+import styles from './Product.module.css'
+import useCart from '../../hooks/useCart';
 
-const Product = ({ product, onAdd, cartItem, substractItem }) => {
+const Product = ({ product }) => {
+    const { get, addItem, substractItem } = useCart()
+    const cartItem = get(product.id)
+
     return (
         <article className={styles.product}>
             <div className={styles.product__image}>
                 <img src={product.image} alt={product.description} />
             </div>
             <div className={styles.product__info}>
-                <h3><strong>{product.brand}</strong> {product.description}</h3>
+                <p><strong>{product.brand}</strong> {product.description}</p>
                 <strong>{currencyFormat(product.price)}</strong>
             </div>
             <div className={styles.product__actions}>
@@ -19,7 +23,7 @@ const Product = ({ product, onAdd, cartItem, substractItem }) => {
                             <MinusIcon size={12} />
                         </button>
                         <span>{cartItem.__quantity}</span>
-                        <button onClick={() => onAdd(product)}>
+                        <button onClick={() => addItem(product)}>
                             <PlusIcon size={12} />
                         </button>
                     </div>
@@ -27,7 +31,7 @@ const Product = ({ product, onAdd, cartItem, substractItem }) => {
                         <div>
                             <button
                                 className={styles.product__add_button}
-                                onClick={() => onAdd(product)}
+                                onClick={() => addItem(product)}
                             >Agregar</button>
                         </div>
                     )}
--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,25 +1,18 @@
 import React from 'react'
 import styles from './Home.module.css'
 import Product from '../../components/product/Product'
+import useFetchProducts from '../../hooks/useFetchProducts'
+import useFetchDiscounts from '../../hooks/useFetchDiscounts'
 
-function Home({ addItem, getItem, substractItem }) {
-    const [products, setProducts] = React.useState([])
-    const [discounts, setDiscounts] = React.useState([])
-
-    React.useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}/api/v1/products`)
-            .then(response => response.json())
-            .then(data => setProducts(data.data))
-        fetch(`${process.env.REACT_APP_API_URL}/api/v1/discounts`)
-            .then(response => response.json())
-            .then(data => setDiscounts(data.data))
-    }, [])
+function Home() {
+    const { items: products } = useFetchProducts()
+    const { items: discounts } = useFetchDiscounts()
 
     return (
         <div>
             <section className={styles.area_products}>
                 {products.length > 0 ? products.map((e, i) => (
-                    <Product substractItem={substractItem} cartItem={getItem(e.id)} key={i} onAdd={addItem} product={e} />
+                    <Product key={i} product={e} />
                 )) : (
                         <div>Loading...</div>
                     )}
