db274a5fbc03f81b3fca5a3c553cd1669c19d01c
Claudio Rojas <cerojasr@falabella.cl>
2021-01-17 03:04:34 -0300

    Added items group by brand into cart store module

--- src/store/cart/index.js
+++ src/store/cart/index.js
@@ -1,65 +1,35 @@
-const removeItem = (cartItems, product) => {
-    let prevItems = cartItems
-    const itemExist = prevItems.find(i => i.id === product.id)
-    if (itemExist) {
-        return cartItems.filter(ci => ci.id !== product.id)
-    }
-}
-
-const substractItem = (cartItems, product) => {
-    let prevItems = cartItems
-    const itemExist = prevItems.find(i => i.id === product.id)
-
-    if (itemExist) {
-        if (itemExist.__quantity === 1) {
-            return removeItem(cartItems, product)
-        }
-        return cartItems.map(ci => ci.id === product.id ? ({
-            ...ci,
-            __quantity: ci.__quantity - 1
-        }) : ({
-            ...ci
-        }))
-    }
-}
-
-const addItem = (cartItems, product) => {
-    let prevItems = cartItems
-    const itemExist = prevItems.find(i => i.id === product.id)
-    if (itemExist) {
-        return cartItems.map(ci => ci.id === product.id ? ({
-            ...ci,
-            __quantity: ci.__quantity + 1
-        }) : ({
-            ...ci
-        }))
-    }
-    return [...cartItems, ({
-        ...product,
-        __quantity: 1
-    })]
-}
+import { addItem, substractItem, removeItem, groupByBrand } from './utils'
 
 const initialState = {
     items: [],
+    itemsByBrand: {
+        items: [],
+        calcTotalByBrand: 0,
+    }
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'CART__ADD_PRODUCT':
+            const itemsAdd = addItem(state.items, action.payload)
             return {
                 ...state,
-                items: addItem(state.items, action.payload),
+                itemsByBrand: groupByBrand(itemsAdd),
+                items: itemsAdd,
             }
         case 'CART__SUBSTRACT_PRODUCT':
+            const itemsSubsctract = substractItem(state.items, action.payload)
             return {
                 ...state,
-                items: substractItem(state.items, action.payload),
+                itemsByBrand: groupByBrand(itemsSubsctract),
+                items: itemsSubsctract,
             }
         case 'CART__REMOVE_PRODUCT':
+            const itemsRemove = removeItem(state.items, action.payload)
             return {
                 ...state,
-                items: removeItem(state.items, action.payload),
+                itemsByBrand: groupByBrand(itemsRemove),
+                items: itemsRemove,
             }
         default:
             return state
