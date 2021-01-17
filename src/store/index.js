75a310fd0ac19fc433d17cceba2eb801781071f7
Claudio Rojas <cerojasr@falabella.cl>
2021-01-17 02:23:38 -0300

    Register discount module store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from 'redux'
 import cartReducer from './cart'
+import discountsReducer from './discounts'
 
 const rootReducer = combineReducers({
     cart: cartReducer,
+    discounts: discountsReducer,
 })
 
 const store = createStore(rootReducer)
