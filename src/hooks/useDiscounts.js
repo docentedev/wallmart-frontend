4ad489183c147a1269fd012bace41d188096bea3
Claudio Rojas <cerojasr@falabella.cl>
2021-01-17 04:51:08 -0300

    Fix error when brand not exist into discount array

--- src/hooks/useDiscounts.js
+++ src/hooks/useDiscounts.js
@@ -5,31 +5,35 @@ const useFetchDiscounts = () => {
     const applyDiscounts = (itemsByBrand) => {
         let totalCalcWithDiscount = 0
         const ibb = itemsByBrand.calcTotalByBrand
+        const out = {}
         if (ibb) {
+
             Object.keys(ibb).forEach((key) => {
-                const discount = discounts.find(discount => discount.brand === key)
-                const itemDiscount = ibb[key]
-                const discountApplied = itemDiscount.total >= discount.threshold
-                if (discount) {
-                    ibb[key] = {
-                        ...itemDiscount,
-                        hasDiscount: true,
-                        discount: discount.discount,
-                        threshold: discount.threshold,
-                        missingForDiscount: discountApplied ? 0 : discount.threshold - itemDiscount.total,
-                        discountApplied: discountApplied,
-                        totalWithDiscount: discountApplied ? itemDiscount.total - discount.discount : itemDiscount.total,
+                if (key) {
+                    const discount = discounts.find(discount => discount.brand === key)
+                    if (discount) {
+                        const itemDiscount = ibb[key]
+                        const discountApplied = itemDiscount.total >= discount.threshold
+                        out[key] = {
+                            ...itemDiscount,
+                            hasDiscount: true,
+                            discount: discount.discount,
+                            threshold: discount.threshold,
+                            missingForDiscount: discountApplied ? 0 : discount.threshold - itemDiscount.total,
+                            discountApplied: discountApplied,
+                            totalWithDiscount: discountApplied ? itemDiscount.total - discount.discount : itemDiscount.total,
+                        }
                     }
                 }
             })
 
-            Object.keys(ibb).forEach((key) => {
-                totalCalcWithDiscount += ibb[key].totalWithDiscount
+            Object.keys(out).forEach((key) => {
+                totalCalcWithDiscount += out[key].totalWithDiscount
             })
         }
 
         return {
-            discounts: ibb,
+            discounts: out,
             total: totalCalcWithDiscount,
         }
     }
