e1726136d1d30a8905dd407ba307eecfc6e0447a
Claudio Rojas <cerojasr@falabella.cl>
2021-01-18 14:45:24 -0300

    Fix Router test

--- src/__test__/Router.test.js
+++ src/__test__/Router.test.js
@@ -4,7 +4,7 @@ import TestRenderer from 'react-test-renderer'
 import Router from '../Router'
 
 describe('Router test', () => {
-    it('should render Router component', () => {
+    it('should render Router component without error', () => {
         const testRenderer = TestRenderer.create(<Router />)
         expect(testRenderer.toJSON()).toMatchSnapshot()
     })
